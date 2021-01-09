8cebdbf131eacb4df4217b661b4ff678d5951abf
Gabbytam <gtam2@fordham.edu>
2021-01-08 16:50:49 -0800

    basic carousel set up

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,30 @@ import React from 'react'
 //component import
 import Project from '../components/common/Project'
 
+//import carousel from package
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 function Projects() {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     //have an object of all the info for my 3 projects 
     //map through that and create a Project component for each one
     const projectInfo = {
@@ -23,7 +46,12 @@ function Projects() {
     return (
         <div id='projects'>
             <h1>Projects</h1>
-            <Project img={projectInfo.uno.image} liveSiteUrl = {projectInfo.uno.liveSite} githubUrl={projectInfo.uno.github}/>
+            <Carousel responsive={responsive}>
+                <div><Project img={projectInfo.uno.image} liveSiteUrl = {projectInfo.uno.liveSite} githubUrl={projectInfo.uno.github}/></div>
+                <div>Project 2</div>
+                <div>Project 3</div>
+            </Carousel>
+            
             
         </div>
     )
