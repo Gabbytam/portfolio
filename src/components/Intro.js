ef8f22a131155b97aa3b2512ca22e0b07c8cd54f
Gabbytam <gtam2@fordham.edu>
2021-02-03 12:45:49 -1000

    changing word on intro page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 // import '../css/stars.css'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function Intro() {
+
+    const [descriptor, setDescriptor] = useState('building projects');
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        changeDescriptor();
+    }, [index])
+
+
+    let delay; 
+
+    const changeDescriptor = () => {
+        let others = ['debugging', 'problem solving', 'analytical thinking', 'group work', 'learning', 'building projects'];
+            delay = setTimeout(() => {
+
+                setDescriptor(others[index])
+                if(index < others.length - 1 ) {
+                    let newIndex = index + 1;
+                    setIndex(newIndex);
+                } else {
+                    setIndex(0);
+                }
+            }, 2000)
+    }
+
+    const stopChangeDescriptor = () => {
+        clearTimeout(delay)
+    }
+
     return (
         <div id='intro'>
             <div id='background'></div>
             <div id='midground'></div>
             <div id='foreground'></div>
             <div id='intro-left'>
-                <h1>Hi, I'm <span id='name'>Gabrielle Tam</span> and welcome to my portfolio.</h1>
+                <h1>Hi, I'm <span className='intro-name'>Gabrielle Tam</span> and welcome to my portfolio.</h1>
 
             </div>
 
             <div id='intro-right'>
-                <p id='intro-text'>I am a software engineer who has found a passion in problem solving.</p>
-
+                <p id='intro-text' onClick = {stopChangeDescriptor}>I am a software engineer who loves </p>
+                <p id='descriptor'><span className='intro-name'>{descriptor}</span>.</p>
             </div>
             
         </div>
