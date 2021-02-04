c3b77c1eabb45466f6fd25af909a19cb4fd9b4df
Gabbytam <gtam2@fordham.edu>
2021-02-03 14:32:26 -1000

    getting rid of extra files

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 // Components imports
-import GitHub from "./components/GitHub";
 import Home from './components/Home'
-import LayoutM from "./components/common/LayoutM";
 import Layout from './components/common/Layout'
-// import About from './components/About'
-// import Projects from './components/Projects'
-// import Resume from './components/Resume'
-import Intro from './components/Intro'
-import Nav from './components/common/Nav'
+
 
 // CSS imports
 import "./css/App.css";
@@ -23,9 +17,6 @@ const App = () => {
       {/* <Intro /> */}
       <Layout>
         <Route exact path='/' component={Home}/>
-        {/* <Route path="/about" component={About}></Route>
-        <Route path="/projects" component={Projects}></Route>
-        <Route path="/resume" component={Resume}></Route> */}
       </Layout>
     </div>
   );
